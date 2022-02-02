c963cafbfc76ecc54e84cf42c99e059343da258a
Louise Braithwaite <louise.braithwaite@newcastle.ac.uk>
2022-02-02 14:57:59 +0000

    add run2015 script

---
+++ run2015.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(LabourMarketAreas)
+library(here)
+
+
+input_data_2015 <- read_delim(here::here("data/input_data/2015/ttwalsoa.csv"),
+                              delim = ";",
+                              escape_double = FALSE,
+                              trim_ws = TRUE) %>%
+  janitor::clean_names()
+
+input_data_2015 <- input_data_2015 %>%
+  mutate(community_live = as.integer(as.factor(living)),
+         community_work = as.integer(as.factor(working))) %>%
+  rename(amount = totalmoves) %>%
+  select(-c("living", "working"))
+
+system.time(
+  output_2015_data  <- findClusters(LWCom = input_data_2015,
+                                    minSZ = 3500,
+                                    minSC= 0.6667,
+                                    tarSZ = 25000,
+                                    tarSC = 0.75)
+)
+
+saveRDS(output_2015_data, "output_2015_data.RDS")
